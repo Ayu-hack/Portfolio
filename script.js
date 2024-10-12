7897561d798bd724a48a819daa0b143ec938c3d4
priyagupta20044 <121642923+priyagupta20044@users.noreply.github.com>
2024-10-13 02:57:07 +0530

    Added new features

---
+++ script.js
@@ -0,0 +1,34 @@
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+const body = document.body;
+
+function darkMode() {
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+}
+
+function lightMode() {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+}
+
+// Optionally, store the preference in localStorage so that the theme persists on page reload
+window.addEventListener("DOMContentLoaded", (event) => {
+    if (localStorage.getItem("theme") === "dark") {
+        darkMode();
+    } else {
+        lightMode();
+    }
+});
+
+
+function toggleTheme() {
+    if (document.body.classList.contains("dark-mode")) {
+        lightMode();
+        localStorage.setItem("theme", "light");
+    } else {
+        darkMode();
+        localStorage.setItem("theme", "dark");
+    }
+}
+themeToggle.addEventListener('click', toggleTheme);
